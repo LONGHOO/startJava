b1428258c651b7536bb7b57dc5a3dc6f896a3d42
shiyi <1144258019@qq.com>
2019-01-13 22:54:27 +0800

    test commit

---
+++ src/com/longhoo/test.java
@@ -0,0 +1,8 @@
+package com.longhoo;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("args = [" + args + "]");
+    }
+}
